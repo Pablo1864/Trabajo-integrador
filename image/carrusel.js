e5a7946f1a1647446960600fc27628f3c67670da
Pablo1864 <pablonpenaloza@sanluis.edu.ar>
2023-06-13 07:24:59 -0300

    x

--- image/carrusel.js
+++ image/carrusel.js
@@ -1,53 +1,195 @@
-const carruseles = [
-  {
-    id: 'carousel1',
-    imagenes: ['car1.jpg', 'car2.jpg', 'descarga.jpg'],
-    indiceActual: 0
-  },
-  {
-    id: 'carousel2',
-    imagenes: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
-    indiceActual: 0
-  }
+ // Array de imágenes para el primer carrusel
+var imagenes = [
+  "duster.jpg",
+  "clio.jpg",
+  "captur.jpg"
 ];
+var informacion = [
+  "Renault Duster",
+  "Renault Clio",
+  "Renault Captur",
+  
+];
+var indiceActual = 0;
+var imagenCarrusel = document.getElementById("imagen-carrusel");
+var infoCarrusel = document.getElementById("info-carrusel");
 
-function mostrarImagen(indice, carrusel) {
-  const contenedor = document.getElementById(carrusel.id);
-  contenedor.innerHTML = ''; // Limpiar el contenido anterior del carrusel
-  const imagen = document.createElement('img');
-  imagen.src = carrusel.imagenes[indice];
-  imagen.classList.add('activo'); // Agregar la clase 'activo' para mostrar la imagen
-  contenedor.appendChild(imagen);
+// Función para cambiar la imagen y la información del carrusel
+function cambiarImagen() {
+  imagenCarrusel.src = imagenes[indiceActual];
+  infoCarrusel.textContent = informacion[indiceActual];
 }
 
-
-function irAImagenAnterior(event) {
-  const carruselId = event.target.parentNode.parentNode.id;
-  const carrusel = carruseles.find((c) => c.id === carruselId);
-  carrusel.indiceActual--;
-  if (carrusel.indiceActual < 0) {
-    carrusel.indiceActual = carrusel.imagenes.length - 1;
+// Función para avanzar a la siguiente imagen
+function siguienteImagen() {
+  indiceActual++;
+  if (indiceActual >= imagenes.length) {
+    indiceActual = 0;
   }
-  mostrarImagen(carrusel.indiceActual, carrusel);
+  cambiarImagen();
 }
 
-function irAImagenSiguiente(carrusel) {
-  carrusel.indiceActual++;
-  if (carrusel.indiceActual >= carrusel.imagenes.length) {
-    carrusel.indiceActual = 0;
+// Función para retroceder a la imagen anterior
+function imagenAnterior() {
+  indiceActual--;
+  if (indiceActual < 0) {
+    indiceActual = imagenes.length - 1;
   }
-  mostrarImagen(carrusel.indiceActual, carrusel);
+  cambiarImagen();
 }
 
+// Cargar la primera imagen y la información al cargar la página
+cambiarImagen();
 
-carruseles.forEach((carrusel) => {
-  mostrarImagen(carrusel.indiceActual, carrusel);
+// Escuchar los clics en los botones de navegación
+document.getElementById("boton-anterior").addEventListener("click", imagenAnterior);
+document.getElementById("boton-siguiente").addEventListener("click", siguienteImagen);
 
-  const botonAnterior = document.querySelector(`#${carrusel.id} .boton-anterior`);
-  const botonSiguiente = document.querySelector(`#${carrusel.id} .boton-siguiente`);
+// Array de imágenes para el segundo carrusel
+var imagenes2 = [
+  "raptor.jpg",
+  "focus.jpg",
+  "fiesta.jpg"
+];
+var informacion2 = [
+  "Ford Raptor",
+  "Ford Focus",
+  "Ford Fiesta",
+  
+];
+var indiceActual2 = 0;
+var imagenCarrusel2 = document.getElementById("imagen-carrusel2");
+var infoCarrusel2 = document.getElementById("info-carrusel2");
 
-  botonAnterior.addEventListener('click', irAImagenAnterior);
-  botonSiguiente.addEventListener('click', irAImagenSiguiente);
-});
+// Función para cambiar la imagen y la información del segundo carrusel
+function cambiarImagen2() {
+  imagenCarrusel2.src = imagenes2[indiceActual2];
+  infoCarrusel2.textContent = informacion2[indiceActual2];
+}
+
+// Función para avanzar a la siguiente imagen del segundo carrusel
+function siguienteImagen2() {
+  indiceActual2++;
+  if (indiceActual2 >= imagenes2.length) {
+    indiceActual2 = 0;
+  }
+  cambiarImagen2();
+}
+
+// Función para retroceder a la imagen anterior del segundo carrusel
+function imagenAnterior2() {
+  indiceActual2--;
+  if (indiceActual2 < 0) {
+    indiceActual2 = imagenes2.length - 1;
+  }
+  cambiarImagen2();
+}
+
+// Cargar la primera imagen y la información del segundo carrusel al cargar la página
+cambiarImagen2();
+
+// Escuchar los clics en los botones de navegación del segundo carrusel
+document.getElementById("boton-anterior2").addEventListener("click", imagenAnterior2);
+document.getElementById("boton-siguiente2").addEventListener("click", siguienteImagen2);
+
+// Array de imágenes para el tercer carrusel
+var imagenes3 = [
+  "onix.jpg",
+  "cruze.jpg",
+  "joy.webp"
+  
+];
+var informacion3 = [
+  "Chevrolet Onix",
+  "Chrevrolet Cruze",
+  "Chevrolet Joy",
+ 
+];
+var indiceActual3 = 0;
+var imagenCarrusel3 = document.getElementById("imagen-carrusel3");
+var infoCarrusel3 = document.getElementById("info-carrusel3");
+
+// Función para cambiar la imagen y la información del tercer carrusel
+function cambiarImagen3() {
+  imagenCarrusel3.src = imagenes3[indiceActual3];
+  infoCarrusel3.textContent = informacion3[indiceActual3];
+}
+
+// Función para avanzar a la siguiente imagen del tercer carrusel
+function siguienteImagen3() {
+  indiceActual3++;
+  if (indiceActual3 >= imagenes3.length) {
+    indiceActual3 = 0;
+  }
+  cambiarImagen3();
+}
+
+// Función para retroceder a la imagen anterior del tercer carrusel
+function imagenAnterior3() {
+  indiceActual3--;
+  if (indiceActual3 < 0) {
+    indiceActual3 = imagenes3.length - 1;
+  }
+  cambiarImagen3();
+}
+
+// Cargar la primera imagen y la información del tercer carrusel al cargar la página
+cambiarImagen3();
+
+// Escuchar los clics en los botones de navegación del tercer carrusel
+document.getElementById("boton-anterior3").addEventListener("click", imagenAnterior3);
+document.getElementById("boton-siguiente3").addEventListener("click", siguienteImagen3);
+
+// Array de imágenes para el cuarto carrusel
+var imagenes4 = [
+  "saveiro.webp",
+  "amarok.webp",
+  "Vento.webp"
+ 
+];
+var informacion4 = [
+  "Volkswagen Saveiro" ,
+  "Volkswagen Amarok",
+  "Volkswagen Vento",
+ 
+];
+var indiceActual4 = 0;
+var imagenCarrusel4 = document.getElementById("imagen-carrusel4");
+var infoCarrusel4 = document.getElementById("info-carrusel4");
+
+// Función para cambiar la imagen y la información del cuarto carrusel
+function cambiarImagen4() {
+  imagenCarrusel4.src = imagenes4[indiceActual4];
+  infoCarrusel4.textContent = informacion4[indiceActual4];
+}
+
+// Función para avanzar a la siguiente imagen del cuarto carrusel
+function siguienteImagen4() {
+  indiceActual4++;
+  if (indiceActual4 >= imagenes4.length) {
+    indiceActual4 = 0;
+  }
+  cambiarImagen4();
+}
+
+// Función para retroceder a la imagen anterior del cuarto carrusel
+function imagenAnterior4() {
+  indiceActual4--;
+  if (indiceActual4 < 0) {
+    indiceActual4 = imagenes4.length - 1;
+  }
+  cambiarImagen4();
+}
+
+// Cargar la primera imagen y la información del cuarto carrusel al cargar la página
+cambiarImagen4();
+
+// Escuchar los clics en los botones de navegación del cuarto carrusel
+document.getElementById("boton-anterior4").addEventListener("click", imagenAnterior4);
+document.getElementById("boton-siguiente4").addEventListener("click", siguienteImagen4);
+
+
+
+
 
 
---
+++ image/formulario.js
@@ -0,0 +1,117 @@
+function validarFormulario() {
+  const nombre = document.getElementById('nombre').value;
+  const email = document.getElementById('email').value;
+  const telefono = document.getElementById('telefono').value;
+  const ciudad = document.getElementById('ciudad').value;
+  const pais = document.getElementById('pais').value;
+  const vehiculo = document.getElementById('mensaje').value;
+
+  // Expresiones regulares para validar el número de teléfono y el formato del correo electrónico
+  const regexTelefono = /^\d{10}$/; // 10 dígitos
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Eliminar los mensajes de error existentes
+  limpiarMensajesError();
+
+  if (nombre === '') {
+    mostrarMensajeError('nombre', 'El campo Nombre es obligatorio.');
+    return false;
+  }
+
+  if (email === '') {
+    mostrarMensajeError('email', 'El campo Email es obligatorio.');
+    return false;
+  }
+
+  if (!regexEmail.test(email)) {
+    mostrarMensajeError('email', 'El formato del correo electrónico es inválido.');
+    return false;
+  }
+
+  if (telefono === '') {
+    mostrarMensajeError('telefono', 'El campo Teléfono es obligatorio.');
+    return false;
+  }
+
+  if (!regexTelefono.test(telefono)) {
+    mostrarMensajeError('telefono', 'El número de teléfono es inválido.');
+    return false;
+  }
+
+  if (ciudad === '') {
+    mostrarMensajeError('ciudad', 'El campo Ciudad es obligatorio.');
+    return false;
+  }
+
+  if (pais === '') {
+    mostrarMensajeError('pais', 'El campo País es obligatorio.');
+    return false;
+  }
+
+  if (mensaje === '') {
+    mostrarMensajeError('mensaje', 'El campo Mensaje es obligatorio.');
+    return false;
+  }
+
+  mostrarDatosEnviados(nombre, email, telefono, ciudad, pais, mensaje);
+  limpiarFormulario();
+  return false;
+}
+
+function mostrarMensajeError(campo, mensaje) {
+  const campoInput = document.getElementById(campo);
+  campoInput.classList.add('campo-error');
+
+  const mensajeErrorExistente = document.querySelector(`#${campo} + .mensaje-error-formulario`);
+  if (mensajeErrorExistente) {
+    mensajeErrorExistente.remove();
+  }
+
+  const mensajeError = document.createElement('p');
+  mensajeError.textContent = mensaje;
+  mensajeError.classList.add('mensaje-error');
+  mensajeError.classList.add('mensaje-error-formulario');
+
+  campoInput.parentNode.insertBefore(mensajeError, campoInput.nextSibling);
+}
+
+function limpiarMensajesError() {
+  const mensajesError = document.querySelectorAll('.mensaje-error-formulario');
+  mensajesError.forEach((mensajeError) => {
+    mensajeError.remove();
+  });
+
+  const camposError = document.querySelectorAll('.campo-error');
+  camposError.forEach((campoError) => {
+    campoError.classList.remove('campo-error');
+  });
+}
+
+function mostrarDatosEnviados(nombre, email, telefono, ciudad, pais, mensaje) {
+  const datosEnviados = document.createElement('div');
+  datosEnviados.innerHTML = `
+    <h2>Datos enviados:</h2>
+    <p><strong>Nombre:</strong> ${nombre}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Teléfono:</strong> ${telefono}</p>
+    <p><strong>Ciudad:</strong> ${ciudad}</p>
+    <p><strong>País:</strong> ${pais}</p>
+    <p><strong>Mensaje:</strong> ${mensaje}</p>
+    <h3>En la brevedad se contactara un representante con usted,muchas gracias</h3>
+    `;
+
+  document.getElementById('datosEnviados').innerHTML = '';
+  document.getElementById('datosEnviados').appendChild(datosEnviados);
+}
+
+function limpiarFormulario() {
+  document.getElementById('nombre').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('telefono').value = '';
+  document.getElementById('ciudad').value = '';
+  document.getElementById('pais').value = '';
+  document.getElementById('mensaje').value = '';
+
+  limpiarMensajesError();
+}
+
